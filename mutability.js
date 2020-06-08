7e99dab61c797cd4025c42c913d25eefa07534eb
alseiitov <dossan.alseiit@gmail.com>
2020-06-08 12:32:44 +0600

    mut

--- mutability.js
+++ mutability.js
@@ -1,13 +1,7 @@
-const person = {
-    name: 'Rick',
-    age: 77,
-    country: 'US',
-};
-
 clone1 = Object.assign({}, person);
 clone2 = Object.assign({}, person);
 
-var samePerson = Object.assign({}, person);
+samePerson = person;
 
 person.age++;
 person.country = 'FR';
