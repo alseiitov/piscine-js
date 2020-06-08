0bb1c7e37d5b4583c4ac120afdd0c16be6f715e4
alseiitov <dossan.alseiit@gmail.com>
2020-06-08 12:43:16 +0600

    more or less

---
+++ more-or-less.js
@@ -0,0 +1,15 @@
+function more(foo) {
+    return foo++
+};
+
+function less(foo) {
+    return foo--
+};
+
+function add(foo, bar) {
+    return (foo + bar)
+};
+
+function sub(foo) {
+    return (foo - bar)
+};
