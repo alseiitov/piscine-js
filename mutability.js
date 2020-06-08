69d91e022a57644bc5b898df091f9eb44c249c5c
alseiitov <dossan.alseiit@gmail.com>
2020-06-08 11:39:03 +0600

    mutability

---
+++ mutability.js
@@ -0,0 +1,17 @@
+const person = {
+    name: 'Rick',
+    age: 77,
+    country: 'US',
+};
+
+clone1 = person;
+clone2 = person;
+
+var samePerson = {
+    name: 'Rick',
+    age: 77,
+    country: 'US',
+};
+
+person.age++;
+person.country = 'FR';
