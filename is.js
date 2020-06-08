7930c9185d5ea8dc8f7b1cb28d2a91fcd37a5940
alseiitov <dossan.alseiit@gmail.com>
2020-06-08 16:54:59 +0600

    s

--- is.js
+++ is.js
@@ -1,8 +1,8 @@
-is.num = (foo) => typeof foo == 'number';
-is.nan = (foo) => typeof foo != 'number';
-is.str = (foo) => typeof foo == 'str';
-is.bool = (foo) => typeof foo == 'boolean';
-is.undef = (foo) => typeof foo == 'undefined';
+is.num = (foo) => typeof foo === 'number';
+is.nan = (foo) => foo.isNaN();
+is.str = (foo) => typeof foo === 'str';
+is.bool = (foo) => typeof foo === 'boolean';
+is.undef = (foo) => typeof foo === 'undefined';
 is.def = (foo) => typeof foo != 'undefined';
 is.arr = (foo) => arr.isArray();
 is.truthy = (foo) =>  foo === true;
