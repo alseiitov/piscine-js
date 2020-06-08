97c5c987d7d82ac923aa3d3e2e7270ae989efcae
alseiitov <dossan.alseiit@gmail.com>
2020-06-08 12:37:11 +0600

    mut6

--- mutability.js
+++ mutability.js
@@ -1,7 +1,7 @@
-clone1 = Object.assign({}, person);
-clone2 = Object.assign({}, person);
+var clone1 = Object.assign({}, person);
+var clone2 = Object.assign({}, person);
 
-samePerson = person;
+const samePerson = person;
 
 person.age++;
 person.country = 'FR';
