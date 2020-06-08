3106cd236ec321dc921d72bae7a13b334bddd098
alseiitov <dossan.alseiit@gmail.com>
2020-06-08 14:47:31 +0600

    Number

--- dog-years.js
+++ dog-years.js
@@ -1,20 +1,20 @@
 function dogYears(planet, seconds) {
     switch (planet) {
         case 'earth':
-            return ((seconds / (1 * 31557600)) * 7).toFixed(2).toString()
+            return Number(((seconds / (1 * 31557600)) * 7).toFixed(2))
         case 'mercury':
-            return ((seconds / (0.2408467 * 31557600)) * 7).toFixed(2).toString()
+            return Number(((seconds / (0.2408467 * 31557600)) * 7).toFixed(2))
         case 'venus':
-            return ((seconds / (0.61519726 * 31557600)) * 7).toFixed(2).toString()
+            return Number(((seconds / (0.61519726 * 31557600)) * 7).toFixed(2))
         case 'mars':
-            return ((seconds / (1.8808158 * 31557600)) * 7).toFixed(2).toString()
+            return Number(((seconds / (1.8808158 * 31557600)) * 7).toFixed(2))
         case 'jupiter':
-            return ((seconds / (11.862615 * 31557600)) * 7).toFixed(2).toString()
+            return Number(((seconds / (11.862615 * 31557600)) * 7).toFixed(2))
         case 'saturn':
-            return ((seconds / (29.447498 * 31557600)) * 7).toFixed(2).toString()
+            return Number(((seconds / (29.447498 * 31557600)) * 7).toFixed(2))
         case 'uranus':
-            return ((seconds / (84.016846 * 31557600)) * 7).toFixed(2).toString()
+            return Number(((seconds / (84.016846 * 31557600)) * 7).toFixed(2))
         case 'neptune':
-            return ((seconds / (164.79132 * 31557600)) * 7).toFixed(2).toString()
+            return Number(((seconds / (164.79132 * 31557600)) * 7).toFixed(2))
     }
 }
