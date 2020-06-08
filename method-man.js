bc36a13316d44a07d3425f78921f4824e11db3cb
alseiitov <dossan.alseiit@gmail.com>
2020-06-08 15:41:50 +0600

    methof-man

--- method-man.js
+++ method-man.js
@@ -2,4 +2,15 @@ const words = (str) => str.split(' ');
 const sentence = (array) => array.join(' ');
 const yell = (str) => str.toUpperCase();
 const whisper = (str) => '*' + str.toLowerCase() + '*';
-const capitalize = (str) => str[0].toUpperCase()+str.slice(1)
+
+function capitalize(str) {
+    let arr = [...str]
+    for (let i = 0; i < arr.length; i++) {
+        if (i == 0) {
+            arr[i] = arr[i].toUpperCase()
+        } else {
+            arr[i] = arr[i].toLowerCase()
+        }
+    }
+    return arr.join('')
+}
