483e0f0ef53d5011d5b07a6c1c2cbaa7d1ebfd2f
alseiitov <dossan.alseiit@gmail.com>
2020-06-10 14:04:48 +0600

    index-of

---
+++ index-of.js
@@ -0,0 +1,29 @@
+function indexOf(array, value) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === value) {
+            return i
+        }
+    }
+    return -1
+}
+
+function lastIndexOf(array, value) {
+    let last = -1
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === value) {
+            last = i
+        }
+    }
+    return last
+}
+
+function includes(array, value) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === value) {
+            return true
+        }
+    }
+    return false
+}
+
+console.log(includes([1,2,2,4], {}))
