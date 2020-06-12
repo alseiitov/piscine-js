fc7b71a68d0a76033ee37a85344be31e59ebcd41
alseiitov <dossan.alseiit@gmail.com>
2020-06-12 14:04:22 +0600

    ion-out

--- ion-out.js
+++ ion-out.js
@@ -1,6 +1,6 @@
 function ionOut(str) {
     const regex = /tion(,| |$)/;
-    let arr = str.split(', ');
+    let arr = str.split(', ').join('').split(' ');
     let res = [];
 
     for (let i = 0; i < arr.length; i++) {
@@ -11,3 +11,5 @@ function ionOut(str) {
     }
     return res
 }
+
+console.log(ionOut(' 1st position is the vision of the 2nd position'))
