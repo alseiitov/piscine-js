329795d80e4f448bf1fc7271b053fe119f707cbf
alseiitov <dossan.alseiit@gmail.com>
2020-06-12 10:41:22 +0600

    g>

--- its-a-match.js
+++ its-a-match.js
@@ -1,4 +1,6 @@
-const normal = /(hi)/i;
-const begin = /^(hi)/i;
-const end = /(hi)$/i;
-const beginEnd = /^(hi).+(hi)$/i;
+const normal = /(hi)/;
+const begin = /^(hi)/;
+const end = /(hi)$/;
+const beginEnd = /^(hi).+(hi)$/;
+
+console.log(normal.test('Hi'))
