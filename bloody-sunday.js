4793af46490399af698c20616a0a6f65233ab124
alseiitov <dossan.alseiit@gmail.com>
2020-06-15 21:19:51 +0600

    remove unused variable

--- bloody-sunday.js
+++ bloody-sunday.js
@@ -8,7 +8,6 @@ function bloodySunday(date) {
         'Saturday'
     ]
     let first = new Date('0001-01-01')
-    let weekDayNum = date.getDay()
-    let week = Math.ceil(((date - first) / 86400000) % 6 );
-    return weekDays[week]
+    let n = Math.ceil(((date - first) / 86400000) % 6 );
+    return weekDays[n]
 }
