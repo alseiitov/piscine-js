4c9a4b64282eabc0ffcba96fb12ca720a8cdb705
alseiitov <dossan.alseiit@gmail.com>
2020-06-20 14:51:11 +0600

    keycodes-symphony

--- keycodes-symphony.js
+++ keycodes-symphony.js
@@ -2,7 +2,7 @@ export { compose }
 
 function compose() {
     document.addEventListener('keyup', event => {
-        if ((/^[a-z]$/).test(event.key)) {
+        if ((/^([a-z]| )$/).test(event.key)) {
             let newDiv = document.createElement('div')
             newDiv.className = 'note'
             newDiv.textContent = event.key
