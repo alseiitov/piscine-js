4792f1d6788b59d947eec3c67212eec63ddae0fb
alseiitov <dossan.alseiit@gmail.com>
2020-06-20 14:54:08 +0600

    keycodes-symphony

--- keycodes-symphony.js
+++ keycodes-symphony.js
@@ -5,7 +5,7 @@ function compose() {
         if ((/^([a-z]| )$/).test(event.key)) {
             let newDiv = document.createElement('div')
             newDiv.className = 'note'
-            newDiv.textContent = event.key
+            newDiv.innerText = event.key
             let color = event.keyCode - 65
             newDiv.style.backgroundColor = `rgb(${color}, ${color}, ${color})`
             document.body.appendChild(newDiv)
@@ -14,7 +14,7 @@ function compose() {
             document.body.lastChild.remove()
         }
         if (event.key === 'Escape') {
-            document.body.textContent = ''
+            document.body.innerText = ''
         }
     })
 }
