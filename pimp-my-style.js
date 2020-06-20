083fe494235c7212610fdb6c1b38a2c12d2d5719
alseiitov <dossan.alseiit@gmail.com>
2020-06-20 12:14:58 +0600

    pimp my style

---
+++ pimp-my-style.js
@@ -0,0 +1,25 @@
+import { styles } from './data.js'
+
+export { pimp }
+
+function pimp() {
+    let button = document.getElementsByClassName('button')[0]
+    let len = button.classList.length
+    let toAddOrDelete = styles[len - 1]
+
+    if (len === 1) {
+        button.textContent = 'pimp my style'
+    }
+ 
+    if (len === styles.length && button.textContent != 'Unpimp my style') {
+        button.textContent = 'Unpimp my style'
+        button.classList.add(toAddOrDelete)
+    }
+    if (button.textContent === 'pimp my style') {
+        button.classList.add(toAddOrDelete)
+    } 
+    if (button.textContent === 'Unpimp my style') {
+        button.classList.remove(button.classList[len-1])
+    } 
+
+}
