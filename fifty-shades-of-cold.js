89e4a6045018adcdffd8a6b9c6f07935cd6b55b5
alseiitov <dossan.alseiit@gmail.com>
2020-06-20 13:49:47 +0600

    fifthy shades of cold

---
+++ fifty-shades-of-cold.js
@@ -0,0 +1,31 @@
+import { colors } from './data.js'
+
+export { generateClasses, generateColdShades, choseShade }
+
+function generateClasses() {
+    let style = document.createElement('style')
+    colors.forEach(color => {
+        style.textContent += (`.${color}{background: ${color};}\n`)
+    });
+    document.head.appendChild(style)
+}
+
+function generateColdShades() {
+    colors.forEach(color => {
+        if ((/(aqua|blue|turquoise|green|cyan|navy|purple)/).test(color)) {
+            let div = document.createElement('div')
+            div.classList.add('card', color)
+            div.textContent = color
+            document.body.appendChild(div)
+        }
+    });
+}
+
+function choseShade(shade) {
+    let allCards = Array.from(document.getElementsByClassName('card'))
+    allCards.forEach(div => {
+        div.classList.remove(div.classList[div.classList.length - 1])
+        div.classList.add(shade)
+    })
+}
+
